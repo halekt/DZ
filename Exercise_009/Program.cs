0e69030e40ca19339c0b1cd556deb8eab88fdeee
unknown <ivfadeev.ekb@gmail.com>
2022-10-07 21:39:12 +0500

    Нахождение индекса элемента

---
+++ Exercise_009/Program.cs
@@ -0,0 +1,20 @@
+﻿// Нахождение индекса элемента 
+
+int [] array =  {1, 2 , 5 , 8 , 10 , 12};
+
+int n = array.Length;
+
+int find = 10; // 10 - число которое нужно найти в массиве
+
+int index = 0;
+
+while(index < n )
+{
+    if(array[index] ==find) // находит индекс заданного числа
+    {
+        Console.WriteLine(index); // показывает индекс найденного числа
+    }
+
+    index++;
+
+}
